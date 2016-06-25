828f3a2ae309f67724a3e406df33ad510db90ab5
Maksimenkova Olga <blackviolet@mail.ru>
2016-06-26 00:01:16 +0300

    standard output

---
+++ Examples/Day_2/InputOutput_01/Source.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+using namespace std;
+////
+int main()
+{
+	int x; // декларация
+	cout << "Enter integer value:\n";
+	cin >> x; // получение значения переменной
+	cout << "\n\nx: " << x;
+	return 0;
+}
